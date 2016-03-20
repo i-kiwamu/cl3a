81380c11ce158bc8ced9eddb9badac6de09e310e
Kiwamu Ishikura <ishikura.kiwamu@gmail.com>
2016-03-20 22:21:37 +0900

    delete unnecessary loop
    delete unnecessary loop

--- src/add_vector.lisp
+++ src/add_vector.lisp
@@ -16,57 +16,53 @@
 (in-package :cl3a.add-vector)
 
 
-(defmacro v+v-ker (val-type p n nvec va vb a b)
+(defmacro v+v-ker (p n nvec va vb a b vc)
   "Add two vectors between a*va and b*vb"
-  (with-gensyms (nv iend res i ip)
-    `(let* ((,nv (min ,nvec ,n))
-            (,iend (min ,nvec (the fixnum (+ ,p ,n))))
-            (,res (make-array ,nv :element-type ',val-type)))
-       (declare (type fixnum ,nv ,iend)
-                (type (simple-array ,val-type (*)) ,res))
+  (with-gensyms (iend i ip)
+    `(let ((,iend (min ,nvec (the fixnum (+ ,p ,n)))))
+       (declare (type fixnum ,iend))
        (dotimes-unroll (,i ,p ,iend)
          (let ((,ip (- (the fixnum ,i) ,p)))
            (declare (type fixnum ,ip))
-           (setf (aref ,res ,ip) (+ (* ,a (aref ,va ,i))
-                                    (* ,b (aref ,vb ,i))))))
-       ,res)))
+           (setf (aref ,vc ,ip) (+ (* ,a (aref ,va ,i))
+                                   (* ,b (aref ,vb ,i)))))))))
 
 
 (declaim (inline dv+v-ker)
          (ftype (function (fixnum fixnum fixnum
                            (simple-array double-float (*))
                            (simple-array double-float (*))
-                           double-float double-float)
-                          (simple-array double-float (*)))
+                           double-float double-float
+                           (simple-array double-float (*))))
                 dv+v-ker))
-(defun dv+v-ker (p n nvec va vb a b)
+(defun dv+v-ker (p n nvec va vb a b vc)
   "Add two double-float vectors between a*va and b*vb"
   (declare (optimize (speed 3) (debug 0) (safety 0))
            (type fixnum p n nvec)
            (type double-float a b)
-           (type (simple-array double-float (*)) va vb))
-  (v+v-ker double-float p n nvec va vb a b))
+           (type (simple-array double-float (*)) va vb vc))
+  (v+v-ker p n nvec va vb a b vc))
 
 
 (declaim (inline lv+v-ker)
          (ftype (function (fixnum fixnum fixnum
                            (simple-array long-float (*))
                            (simple-array long-float (*))
-                           long-float long-float)
-                          (simple-array long-float (*)))
+                           long-float long-float
+                           (simple-array long-float (*))))
                 lv+v-ker))
-(defun lv+v-ker (p n nvec va vb a b)
+(defun lv+v-ker (p n nvec va vb a b vc)
   "Add two long-float vectors between va and vb"
   (declare (optimize (speed 3) (debug 0) (safety 0))
            (type fixnum p n nvec)
            (type long-float a b)
-           (type (simple-array long-float (*)) va vb))
-  (v+v-ker long-float p n nvec va vb a b))
+           (type (simple-array long-float (*)) va vb vc))
+  (v+v-ker p n nvec va vb a b vc))
 
 
 (defmacro v+v (val-type fun a va b vb)
   "Add two vectors within L1 cache-size"
-  (with-gensyms (na nb nvec tbl m k vc i res ir ii nk)
+  (with-gensyms (na nb nvec tbl m k vc i nk)
     `(let* ((,na (length ,va))
             (,nb (length ,vb))
             (,nvec (cond ((/= ,na ,nb) (different-length-warn ,na ,nb)
@@ -80,21 +76,11 @@
                 (type (simple-array ,val-type (*)) ,vc))
        (do ((,i 0 (the fixnum (+ ,i ,m))))
            ((>= (the fixnum ,i) ,k))
-         (let ((,res (funcall ,fun ,i ,m ,nvec ,va ,vb ,a ,b)))
-           (declare (type (simple-array ,val-type (*)) ,res))
-           (dotimes (,ir ,m)
-             (let ((,ii (+ ,ir ,i)))
-               (declare (type fixnum ,ii))
-               (setf (aref ,vc ,ii) (aref ,res ,ir))))))
+         (funcall ,fun ,i ,m ,nvec ,va ,vb ,a ,b ,vc))
        (when (> ,nvec ,k)
-         (let* ((,nk (- ,nvec ,k))
-                (,res (funcall ,fun ,k ,nk ,nvec ,va ,vb ,a ,b)))
-           (declare (type fixnum ,nk)
-                    (type (simple-array ,val-type (*)) ,res))
-           (do ((,i ,k (1+ ,i))
-                (,ir 0 (1+ ,ir)))
-               ((>= ,i ,nvec))
-             (setf (aref ,vc ,i) (aref ,res ,ir)))))
+         (let ((,nk (- ,nvec ,k)))
+           (declare (type fixnum ,nk))
+           (funcall ,fun ,k ,nk ,nvec ,va ,vb ,a ,b ,vc)))
        ,vc)))
 
 
